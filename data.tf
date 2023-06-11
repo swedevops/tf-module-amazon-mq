49d876074416143784f58c6b706e26036b419761
swedev99 <swedevops99@gmail.com>
2023-06-11 18:26:39 +0530

    code dry

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "centos" {
+data "aws_ami" "ami" {
   most_recent = true
   owners      = ["973714476881"]
   name_regex  = "Centos-8-DevOps-Practice"
--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "main" {
 
 
 resource "aws_instance" "rabbitmq" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
   subnet_id              = var.subnets[0]
   vpc_security_group_ids = [aws_security_group.main.id]
